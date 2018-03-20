bdc3fad9cbcad67d7c62379c6415e3687cb08d09
Justyna Bogacz <kadijanna@gmail.com>
2018-03-20 20:52:21 +0100

    bravo i fe

--- src/main/kotlin/koty/Main.kt
+++ src/main/kotlin/koty/Main.kt
@@ -1,5 +1,17 @@
 package koty
 
-fun main(a: Array<String>) {
-    print("Hello, World!")
+fun main(xxx: Array<String>) {
+    println("napisz coś")
+    val input = readLine()
+    if (input == "coś") {
+        println("bravo!")
+    } else if (input == "COŚ") {
+        println("wyłącz Caps Locka!")
+    } else if (input == "kurwa" || input == "chuj") {
+        println("nie klnij!")
+    } else if (input == "chuj") {
+        println("chyba ty!")
+    } else {
+        println("fe!")
+    }
 }
